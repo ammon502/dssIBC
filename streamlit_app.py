0bbc5b42951bfbcc77974845245f50dba7592aa5
DerekDoelling <167832480+DerekDoelling@users.noreply.github.com>
2024-11-06 18:21:06 -0700

    Uploaded the app

---
+++ streamlit_app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import openpyxl
+
+# Set page title and layout
+st.set_page_config(page_title='IBC Data Explorer Dashboard', page_icon='📊', layout='wide')
+
+# Sidebar navigation
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ["Home", "Sales", "Traffic", "Members", "Feedback"])
+
+# Page routing
+if page == "Home":
+    import page.home as home
+    home.app()
+elif page == "Sales":
+    import page.sales as sales
+    sales.app()
+elif page == "Traffic":
+    import page.traffic as traffic
+    traffic.app()
+elif page == "Members":
+    import page.members as members
+    members.app()
+elif page == "Feedback": 
+    import page.feedback as feedback
+    feedback.app()
